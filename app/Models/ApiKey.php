a277c9e9fc49e9ca1ef761c36778e3e9afbb2485
yawwnann <c0478592@gmail.com>
2025-03-08 05:34:28 +0800

    api consume

--- app/Models/ApiKey.php
+++ app/Models/ApiKey.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class ApiKey extends Model
 {
-    //
+    protected $fillable = ['name', 'key']; // Menambahkan key ke fillable
+
+    protected static function booted()
+    {
+        static::creating(function ($apiKey) {
+            $apiKey->key = Str::random(32);  // Menghasilkan string acak untuk 'key'
+        });
+    }
 }
--- app/Models/city.php
+++ app/Models/city.php
@@ -1,26 +1,30 @@
 <?php
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class City extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name',
-        'photo',
         'slug',
+        'photo',
     ];
 
-    // Menambahkan event untuk mengisi slug secara otomatis
-    protected static function booted()
+    public function setNameAttribute($value)
     {
-        static::creating(function ($city) {
-            $city->slug = Str::slug($city->name);
-        });
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function officeSpaces(): HasMany
+    {
+        return $this->hasMany(OfficeSpace::class);
     }
 }
